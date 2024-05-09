4a37ce5fba39ef8d3dbfb61a729896e0c0c6401b
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-05-09 12:44:23 +0545

    create admin form

--- src/components/admin/adminhome/adminbody.js
+++ src/components/admin/adminhome/adminbody.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import "./adminbody.css";
+import CreateAdminForm from '../createadmin/createadminform';
 
 export default function AdminBody() {
   return (
-    <div className="admin-body">
-      <h2>Admin Panel</h2>
-      <div className="admin-content">
-        {/* Your admin content goes here */}
-        <p>This is the main content area for the admin panel.</p>
-        <p>You can add tables, charts, forms, or other components here.</p>
-      </div>
-    </div>
+    <>
+    <CreateAdminForm />
+    </>
   );
 }
--- src/components/admin/adminhome/adminhome.js
+++ src/components/admin/adminhome/adminhome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AdminSidebar from '../adminsidebar/adminSidebar';
 import AdminBody from './adminbody';
-import './adminhome.css'; // Import CSS file for styling
+import './adminhome.css';
 
 export default function AdminHome() {
   return (
