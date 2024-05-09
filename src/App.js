91ad177c69e30fc676219e1795f2a0f61441495c
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-05-09 09:02:07 +0545

    bug fixes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TopBar from './components/topbar/TopBar';
 import Home from './components/home/home';
 import About from './components/about/About';
-<<<<<<< HEAD
 import Blog from "./components/posts/posts"
 import Singlepost from "./components/singlePost/singlepost"; 
 
-=======
 import Profile from './components/profile/Profile';
 import Changepassword from './components/profile/Changepassword';
->>>>>>> 9b2bc52bb9ccb6f71d28e5c6ce43e48fce502bc9
 
 function App() {
   return (
@@ -18,15 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-<<<<<<< HEAD
         <Route path="/post" element={<Blog />} />
         <Route path="/post/singlePost/singlePOST" element={<Singlepost />} /> 
 
 
-=======
         <Route path="/profile" element={<Profile />} />
         <Route path="/changepassword" element={<Changepassword />} />
->>>>>>> 9b2bc52bb9ccb6f71d28e5c6ce43e48fce502bc9
       </Routes>
     </Router>
   );
