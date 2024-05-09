d00c20e9f2387b36d4a7a93c06aad7682d2841f0
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-05-09 12:24:30 +0545

    udpate in admin sidebar

--- src/components/admin/adminsidebar/adminSidebar.js
+++ src/components/admin/adminsidebar/adminSidebar.js
@@ -1,33 +1,31 @@
 import "./adminSidebar.css";
 import { Link } from 'react-router-dom';
 
-export default function adminSidebar() {
+export default function AdminSidebar() {
   return (
-    <div className="row">
-      <div className="col-lg-4">  
-        <div className="admin-sidebar my-4">
-          <h3>Admin Dashboard</h3>
-          <ul>
-            <li>
-              <Link to="/admin/create-blog">
-                <i className="fas fa-pencil-alt"></i> 
-                Create Blog
-              </Link>
-            </li>
-            <li>
-              <Link to="/admin/create-user">
-                <i className="fas fa-user-plus"></i> 
-                Create User
-              </Link>
-            </li>
-            <li>
-              <Link to="/admin/popular-blogs">
-                <i className="fas fa-chart-line"></i> 
-                Popular Blogs
-              </Link>
-            </li>
-          </ul>
-        </div> 
+    <div className="admin-sidebar-container">
+      <div className="admin-sidebar">
+        <h3>Admin Dashboard</h3>
+        <ul>
+          <li>
+            <Link to="/admin/create-blog">
+              <i className="fas fa-pencil-alt"></i> 
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/create-user">
+              <i className="fas fa-user-plus"></i> 
+              Create Admin
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/popular-blogs">
+              <i className="fas fa-chart-line"></i> 
+              Popular Blogs
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
