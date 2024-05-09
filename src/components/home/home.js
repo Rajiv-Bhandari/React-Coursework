86713d5d22b43a40b93d6f8a1770115a0f37f37a
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-05-09 13:01:57 +0545

    bug fixes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,8 @@
 import Footer from "../footer/footer";
 import Header from "../header/Header";
 import Post from "../posts/posts";
-<<<<<<< HEAD
 import Sidebar from "../sidebar/sidebar";
-import AdminSidebar from "../adminsidebar/AdminSidebar";
-=======
->>>>>>> 5f2b2c55eb5f038d9624dff5cebc98ec862e676e
+import AdminSidebar from "../admin/adminsidebar/adminSidebar";
 import "./home.css";
 import Contact from "../../contact/contact";
 
@@ -16,11 +13,8 @@ export default function home() {
       <div className="home">
         <Post />
         <Sidebar />
-<<<<<<< HEAD
         <AdminSidebar />
         <Contact />
-=======
->>>>>>> 5f2b2c55eb5f038d9624dff5cebc98ec862e676e
       </div>
       <Footer />
     </>
