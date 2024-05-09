106890041661842f0ac5f958b320c19bff4647c2
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-05-09 13:18:29 +0545

    contact form button visible

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,35 +3,28 @@ import "./contact.css";
 export default function contact() {
   return (
     <div className='container'>
-            <div class='contact-section'>
+        <div class='contact-section'>
             <div class="row my-4 justify-content-center">
-                    
-                    <div class="col-lg-6">
-                        <form>
-                            <div class="form-content mb-4">
-                                    <div class="form-details">
-                                        <input type="text" id="customername" name="" placeholder="Full Name"
-                                            class="w-100 mb-4" />
-                                        <input type="email" id="customeremail" name="" placeholder="Email"
-                                            class="w-100 mb-4" />
-                                        <input type="number" id="Customernumber" name="" placeholder="Phone Number"
-                                            class="w-100 mb-4" />
-                                        
-                                       
-                                        <textarea name="message" id="message" cols="60" rows="4"  placeholder="Message"></textarea>
-                                    </div>
-                                    <div class=" form-btn text-center m-3">
-                                        <button>Send Message
-                                            <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                                        </button>
-                                    </div>
+                <div class="col-lg-6">
+                    <form>
+                        <div class="form-content mb-4">
+                                <div class="form-details">
+                                    <input type="text" id="customername" name="" placeholder="Full Name"
+                                        class="w-100 mb-4" />
+                                    <input type="email" id="customeremail" name="" placeholder="Email"
+                                        class="w-100 mb-4" />
+                                    <input type="number" id="Customernumber" name="" placeholder="Phone Number"
+                                        class="w-100 mb-4" />
+                                    
+                                    
+                                    <textarea name="message" id="message" cols="60" rows="4"  placeholder="Message"></textarea>
                                 </div>
-                        </form>
-                    </div>                   
-                </div>
-
+                                <button>Send Message</button>
+                            </div>
+                    </form>
+                </div>                   
             </div>
-
         </div>
+    </div>
   )
 }
