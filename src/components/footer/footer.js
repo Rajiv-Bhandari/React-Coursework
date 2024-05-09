8f2574f45368ba699150c62b47e88ddf1f8af21f
ayhskerp333@gmail.com <152762496+Prekshyas@users.noreply.github.com>
2024-05-09 17:59:17 +0545

    footer created

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,10 +2,39 @@
 
 import React from "react";
 import "./footer.css";
+import { Link } from "react-router-dom";
+
+
 
 function Footer() {
   return (
     <footer>
+       <div className="container">
+        <div className="row justify-content-between my-3">
+        <div className="col-lg-6">
+            <h3>Bilserium</h3>
+            <p>At our social platform, we strive to create a vibrant community where users can connect, share ideas, and engage with each other through blogs, discussions, and more.</p>
+        </div>
+        <div className="col-lg-6">
+          <div className="text-center">
+            <h3>Quick Links</h3>
+            <ul class="footer-links">
+              <li><Link to="/home" className="footerItem">
+                Home
+              </Link></li>
+              <li><Link to="/post" className="footerItem">
+                Blog
+              </Link></li>
+              <li><Link to="/contact" className="footerItem">
+                Contact
+              </Link></li>  
+                        </ul>
+          </div>
+
+        </div>
+        </div>
+
+    </div>
       <p>&copy; 2024 Bislerium Blogs. All rights reserved.</p>
     </footer>
   );
