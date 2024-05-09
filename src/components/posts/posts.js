e695c0d7ed8b24eb454334fc2bfb7020a863636e
ayhskerp333@gmail.com <152762496+Prekshyas@users.noreply.github.com>
2024-05-09 06:43:00 +0545

    side bar image adjusted

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,72 +1,122 @@
-import "./posts.css";
-import Sidebar from "../sidebar/sidebar";
-import Header from "../header/Header"; 
+// import "./posts.css";
+// import Sidebar from "../sidebar/sidebar";
+// import Header from "../header/Header"; 
 
 
-export default function Post() {
-  return(
-  <>
-  <Header />
-   <div className="posts my-3">
-    <div className="row justify-content-between px-2">
-      <div className="col-lg-4 mb-2">
-        <div >
-          <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
-        </div>
-        <p className="blog-topic mb-1">Music Life</p>
-        <h5 className="blog-topic">
-          Lorem ipsum dolor  adipisicing elit.
-        </h5>
-        <p className="blog-topic">1 hour ago</p>
+// export default function Post() {
+//   return(
+//   <>
+//   <Header />
+//    <div className="posts my-3">
+//     <div className="row justify-content-between px-2">
+//       <div className="col-lg-4 mb-2">
+//         <div >
+//           <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
+//         </div>
+//         <p className="blog-topic mb-1">Music Life</p>
+//         <h5 className="blog-topic">
+//           Lorem ipsum dolor  adipisicing elit.
+//         </h5>
+//         <p className="blog-topic">1 hour ago</p>
 
-        <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
-      </div>
-      <div className="col-lg-4">
-        <div >
-          <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
-        </div>
-        <p className="blog-topic mb-1">Music Life</p>
-        <h5 className="blog-topic">
-          Lorem ipsum dolor  adipisicing elit.
-        </h5>
-        <p className="blog-topic">1 hour ago</p>
+//         <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
+//       </div>
+//       <div className="col-lg-4">
+//         <div >
+//           <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
+//         </div>
+//         <p className="blog-topic mb-1">Music Life</p>
+//         <h5 className="blog-topic">
+//           Lorem ipsum dolor  adipisicing elit.
+//         </h5>
+//         <p className="blog-topic">1 hour ago</p>
 
-        <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
-      </div>
-      <div className="col-lg-4">
-        <div >
-          <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
-        </div>
-        <p className="blog-topic mb-1">Music Life</p>
-        <h5 className="blog-topic">
-          Lorem ipsum dolor  adipisicing elit.
-        </h5>
-        <p className="blog-topic">1 hour ago</p>
+//         <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
+//       </div>
+//       <div className="col-lg-4">
+//         <div >
+//           <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
+//         </div>
+//         <p className="blog-topic mb-1">Music Life</p>
+//         <h5 className="blog-topic">
+//           Lorem ipsum dolor  adipisicing elit.
+//         </h5>
+//         <p className="blog-topic">1 hour ago</p>
 
-        <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
-      </div>
-      <div className="col-lg-4">
-        <div >
-          <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
-        </div>
-        <p className="blog-topic mb-1">Music Life</p>
-        <h5 className="blog-topic">
-          Lorem ipsum dolor  adipisicing elit.
-        </h5>
-        <p className="blog-topic">1 hour ago</p>
+//         <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
+//       </div>
+//       <div className="col-lg-4">
+//         <div >
+//           <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
+//         </div>
+//         <p className="blog-topic mb-1">Music Life</p>
+//         <h5 className="blog-topic">
+//           Lorem ipsum dolor  adipisicing elit.
+//         </h5>
+//         <p className="blog-topic">1 hour ago</p>
 
-        <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
-      </div>
-      <div className="col-lg-4">
-  <Sidebar/>
-  </div>
-    </div>
-  </div>
+//         <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
+//       </div>
+//       <div className="col-lg-4">
+//   <Sidebar/>
+//   </div>
+//     </div>
+//   </div>
  
    
 
 
   
-  </>
+//   </>
+//   );
+// }
+import React from 'react';
+import Sidebar from '../sidebar/sidebar';
+import Header from '../header/Header';
+import './posts.css';
+
+export default function Post() {
+  return (
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8">
+            <div className="posts my-3">
+              <div className="row justify-content-between px-2">
+                <div className="col-lg-6 mb-2">
+                  <div >
+                    <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
+                  </div>
+                  <p className="blog-topic mb-1">Music Life</p>
+                  <h5 className="blog-topic">
+                    Lorem ipsum dolor  adipisicing elit.
+                  </h5>
+                  <p className="blog-topic">1 hour ago</p>
+
+                  <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
+                </div>
+                {/* Add another blog content section here */}
+                <div className="col-lg-6">
+                  <div >
+                    <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?t=st=1715174753~exp=1715178353~hmac=154409cdeabb627619eb694fdac90d51b415da63650e5dd0ee975ae597de8bcf&w=1060" alt="blog-img" width="100%" className="rounded-2"/>
+                  </div>
+                  <p className="blog-topic mb-1">Music Life</p>
+                  <h5 className="blog-topic">
+                    Lorem ipsum dolor  adipisicing elit.
+                  </h5>
+                  <p className="blog-topic">1 hour ago</p>
+
+                  <p className="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error officiis dolore omnis. Ullam minima, expedita quibusdam distinctio voluptates sapiente placeat recusandae reiciendis iste, aliquam in. Impedit aspernatur repudiandae optio ex!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <Sidebar />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -6,8 +6,8 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
         <img
-          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-          alt=""
+          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D "
+          alt="" width="100%"
         />
 
         <p>
