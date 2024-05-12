b36755b6374c679646e2765e2eaa6822fee56afa
RiyaShresthaa <riyashrestha279@gmail.com>
2024-05-12 16:54:39 +0545

    login token

--- src/components/loginsignup/login.js
+++ src/components/loginsignup/login.js
@@ -3,10 +3,14 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./login.css";
 import Footer from "../footer/footer";
+import { jwtDecode } from "jwt-decode";
+import Cookies from "universal-cookie";
+
 // import { useHistory } from "react-router-dom"; // Assuming you're using React Router
 
 export default function Login() {
-  const [email, setEmail] = useState("");
+  const cookies = new Cookies();
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   // const history = useHistory();
 
@@ -16,12 +20,13 @@ export default function Login() {
     try {
       const response = await fetch("https://localhost:7186/api/Auth/Login", {
         method: "POST",
+        mode: "cors",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
       });
-
+      console.log(response);
       if (!response.ok) {
         throw new Error("Failed to login");
       }
@@ -30,7 +35,9 @@ export default function Login() {
       console.log("Login successful");
       toast.success("Login successful");
 
-      document.cookie = `x-access-token=${data.token}; path=/;`;
+      const token = data.responseData;
+      const decoded = jwtDecode(token);
+      console.log(decoded);
 
       // if (data.role === "ADMIN") {
       //   history.push("/admin/dashboard");
@@ -41,7 +48,7 @@ export default function Login() {
       // }
     } catch (error) {
       console.error("Error:", error);
-      toast.error("Failed to login");
+      toast.error("Failed to login. OK?");
     }
   };
 
@@ -56,8 +63,8 @@ export default function Login() {
             className="login-input"
             placeholder="Your Email"
             autoComplete="off"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
--- src/components/loginsignup/loginsignup.js
+++ src/components/loginsignup/loginsignup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./loginsignup.css";
 import Footer from "../footer/footer";
 
@@ -13,54 +13,85 @@ export default function Loginsignup() {
     try {
       const response = await fetch("https://localhost:7186/api/Auth/Register", {
         method: "POST",
+        mode: "cors",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ fullName, username, password })
+        body: JSON.stringify({ fullName, username, password }),
       });
-
+      console.log(response);
       if (!response.ok) {
-        throw new Error('Failed to register');
+        throw new Error("Failed to register");
       }
 
-      console.log('Registration successful');
+      console.log("Registration successful");
       // Handle successful registration, such as redirecting the user or displaying a success message
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
     <>
-    <div>
-      <div className="section">
-        <div className="center-container">
-          <div className="row full-height justify-content-center">
-            <div className="col-12 text-center align-self-center py-5">
-              <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                <h6 className="mb-0 pb-3">Sign Up</h6>
-                {/* <input className="checkbox" type="checkbox" id="reg-log" name="reg-log"/> */}
-                <label htmlFor="reg-log"></label>
-                <div className="card-3d-wrap mx-auto">
-                  <div className="card-3d-wrapper">
-                    <div className="card-front">
-                      <div className="center-wrap">
-                        <div className="section text-center">
-                          <form onSubmit={handleSubmit}>
-                            <div className="form-group">
-                              <input type="text" name="fullName" className="form-style" placeholder=" Full Name" id="fullName" autoComplete="off" value={fullName} onChange={(e) => setName(e.target.value)} />
-                              <i className="input-icon uil uil-user"></i>
-                            </div>
-                            <div className="form-group mt-2">
-                              <input type="email" name="username" className="form-style" placeholder=" UserName" id="username" autoComplete="off" value={username} onChange={(e) => setUsername(e.target.value)} />
-                              <i className="input-icon uil uil-at"></i>
-                            </div>
-                            <div className="form-group mt-2">
-                              <input type="password" name="password" className="form-style" placeholder=" Password" id="password" autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} />
-                              <i className="input-icon uil uil-lock-alt"></i>
-                            </div>
-                            <button type="submit" className="btn mt-4">Submit</button>
-                          </form>
+      <div>
+        <div className="section">
+          <div className="center-container">
+            <div className="row full-height justify-content-center">
+              <div className="col-12 text-center align-self-center py-5">
+                <div className="section pb-5 pt-5 pt-sm-2 text-center">
+                  <h6 className="mb-0 pb-3">Sign Up</h6>
+                  {/* <input className="checkbox" type="checkbox" id="reg-log" name="reg-log"/> */}
+                  <label htmlFor="reg-log"></label>
+                  <div className="card-3d-wrap mx-auto">
+                    <div className="card-3d-wrapper">
+                      <div className="card-front">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <form onSubmit={handleSubmit}>
+                              <div className="form-group">
+                                <input
+                                  type="text"
+                                  name="fullName"
+                                  className="form-style"
+                                  placeholder=" Full Name"
+                                  id="fullName"
+                                  autoComplete="off"
+                                  value={fullName}
+                                  onChange={(e) => setName(e.target.value)}
+                                />
+                                <i className="input-icon uil uil-user"></i>
+                              </div>
+                              <div className="form-group mt-2">
+                                <input
+                                  type="email"
+                                  name="username"
+                                  className="form-style"
+                                  placeholder=" UserName"
+                                  id="username"
+                                  autoComplete="off"
+                                  value={username}
+                                  onChange={(e) => setUsername(e.target.value)}
+                                />
+                                <i className="input-icon uil uil-at"></i>
+                              </div>
+                              <div className="form-group mt-2">
+                                <input
+                                  type="password"
+                                  name="password"
+                                  className="form-style"
+                                  placeholder=" Password"
+                                  id="password"
+                                  autoComplete="off"
+                                  value={password}
+                                  onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <i className="input-icon uil uil-lock-alt"></i>
+                              </div>
+                              <button type="submit" className="btn mt-4">
+                                Submit
+                              </button>
+                            </form>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -71,8 +102,7 @@ export default function Loginsignup() {
           </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 }
